54f88fd9d4d5ab62d787b4d1b1688d7b1729b23a
TBiesenbeek <tbiesenbeek@swis.nl>
2024-11-08 15:25:52 +0100

    feat: setup project for mautic with basic sentry impl.

---
+++ MauticSentryBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\MauticSentryBundle;
+
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class MauticSentryBundle extends PluginBundleBase
+{
+}
